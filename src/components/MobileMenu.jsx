2651baba39c23434dc69a9cd16bcd91039e32388
udinesia325 <udinesia325@gmail.com>
2024-02-11 10:00:48 +0700

    navbar movile

---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import navbar from '../data/navbar'
+
+function MobileMenu({ openMenu }) {
+  return (
+    <div
+      className={`absolute -z-10 w-[95%] mx-auto bg-white/80 backdrop-blur-sm border shadow-lg py-3 rounded-md transition-all block sm:hidden ${
+        openMenu ? 'top-20' : '-top-72'
+      }`}
+    >
+      <ul className='flex flex-col gap-3 justify-center items-center'>
+        {navbar.map((nav, index) => (
+          <li key={index}>
+            <a href={nav.href} className='text-sm font-semibold transition-all hover:text-primary-400 hover:tracking-wide'>{nav.title}</a>
+          </li>
+        ))}
+        <li className="btn-primary ml-4 w-[60%] text-center text-sm font-semibold">Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MobileMenu
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import navbar from '../data/navbar'
+import { Icon } from '@iconify/react'
+import MobileMenu from './MobileMenu'
 
 function Navbar() {
+  const [scroll, setScroll] = useState(false)
+  const [openMenu, setOpenMenu] = useState(false)
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        setScroll(true)
+      } else {
+        setScroll(false)
+      }
+    })
+  }, [])
+  const toggleMenu = () => {
+    setOpenMenu((prev) => !prev)
+  }
+
   return (
-    <div className='container max-w-5xl flex justify-between items-center py-4 bg-white sticky top-0 z-50'>
-        <h1 className='font-bold text-3xl'>DINNN</h1>
-        <ul className='flex items-center text-sm font-semibold'>
-            {navbar.map((link,index) => (
-                <li key={index} className='cursor-pointer hover:bg-primary-100/20 py-2 px-3 ml-2'><a href={link.href}>{link.title}</a></li>
-            ))}
-            <li className='btn-primary ml-4'>Contact</li>
-        </ul>
+    <div
+      className={`container max-w-5xl flex justify-between items-center py-4 px-2 sm:px-5 sticky top-0 z-50 transition-all ${
+        scroll ? 'shadow-lg bg-white/80 backdrop-blur-sm' : 'bg-white'
+      }`}
+    >
+      <a href="#" className="font-bold text-3xl">
+        DINNN
+      </a>
+      <ul className="hidden sm:flex items-center text-sm font-semibold">
+        {navbar.map((link, index) => (
+          <li
+            key={index}
+            className="cursor-pointer transition-all hover:text-primary-400 hover:bg-primary-100/20 py-2 px-3 ml-2"
+          >
+            <a href={link.href}>{link.title}</a>
+          </li>
+        ))}
+        <li className="btn-primary ml-4">Contact</li>
+      </ul>
+      <div onClick={toggleMenu} className={`transition-all cursor-pointer block sm:hidden ${openMenu ? '-rotate-180' : 'rotate-0'}`}>
+        {openMenu ?  <Icon icon="mingcute:close-fill" className="text-gray-900 text-3xl" /> :  <Icon icon="gg:menu-right-alt" className="text-gray-900 text-3xl" />}
+      </div>
+
+      <MobileMenu openMenu={openMenu} />
     </div>
   )
 }
